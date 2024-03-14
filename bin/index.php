d0476da66b174e8cf15276f6e9cdb0548ab6b8cb
whoislorex <143047878+whoislorex@users.noreply.github.com>
2024-03-14 23:34:31 +0300

    Create index.php

---
+++ bin/index.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<title>search844</title>
+<meta name="viewport" content="width=device-width, initial-scale=0.5">
+<link rel="icon" href="coffee2.gif" type="image/x-icon" />
+</head>
+<body bgcolor="#19181c">
+<div class="div">
+<button class="login">Log In</button>
+</div>
+<center>
+<br><br>
+<img height="50" src="coffee2.gif"><br>
+<span class="title">search844</span><br><br>
+<input class="search">&nbsp;<input type="submit" value="Search" class="searchbtn">
+</center>
+<style>
+.title {
+    color:pink;
+    font-size:40px;
+    font-family: arial;
+}
+.title2 {
+    color:gray;
+    font-size:27px;
+    font-family: arial;
+}
+.search {
+    background-color:white;
+    color:black;
+    outline: none;
+    border:none;
+    font-size:18px;
+}
+.searchbtn {
+    background-color:white;
+    color:black;
+    outline: none;
+    border:none;
+    font-size:18px;
+    cursor: pointer;
+}
+.login {
+    float:right;
+    color:white;
+    background-color:pink;
+    font-size:22px;
+    cursor: pointer;
+    border-radius:5px;
+    border:none;
+}
+.register {
+    float:right;
+    color:white;
+    background-color:#007fff;
+    font-size:22px;
+    cursor: pointer;
+    border-radius:5px;
+    border:none;
+}
+.div {
+        width: 99%;
+}
+</style>
+</body>
+</html>
